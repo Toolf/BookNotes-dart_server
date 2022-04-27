183bbb170f799878f271d6dab909bb7f77cd16b6
Toolf <nazar.smirnov.2001@gmail.com>
2022-04-27 21:37:28 +0300

    DS-002: add: book read endpoint tests

--- test/unit/api/book_api_test.dart
+++ test/unit/api/book_api_test.dart
@@ -1,5 +1,6 @@
 import 'package:book_notes/api/book/book_api.dart';
 import 'package:book_notes/core/exception/validation_exception.dart';
+import 'package:book_notes/domain/book/book.dart';
 import 'package:book_notes/domain/book/book_create.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -51,6 +52,47 @@ void main() {
         final bookId = await bookApi.create.method(bookCreate);
         // assert
         expect(bookId, expectedBookId);
+      });
+    });
+  });
+
+  group("Read Endpoint:", () {
+    final bookJson = fixture("db/book/book_fixture.json");
+    final book = Book.fromJson(bookJson);
+    group("Validate parameters:", () {
+      test('Given valid book id when validate then returns normaly', () async {
+        // arrange
+        // act
+        // assert
+        expect(
+          () => bookApi.read.parameters!.validate(book.bookId),
+          returnsNormally,
+        );
+      });
+      test('Given invalid book id when validate then throw exception',
+          () async {
+        // arrange
+        final invalidBookIdJson = {};
+        // act
+        // assert
+        expect(
+          () => bookApi.read.parameters!.validate(invalidBookIdJson),
+          throwsA(TypeMatcher<ValidationException>()),
+        );
+      });
+    });
+
+    group("Method:", () {
+      test('Given valid book id when execute method then return book',
+          () async {
+        // arrange
+        final expectedBook = book;
+        when((() => bookDataSource.read(expectedBook.bookId)))
+            .thenAnswer((_) => Future.value(expectedBook));
+        // act
+        final actualBook = await bookApi.read.method(expectedBook.bookId);
+        // assert
+        expect(actualBook, expectedBook);
       });
     });
   });
