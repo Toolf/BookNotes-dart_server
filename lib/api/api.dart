b045265fe1c94a51e366bf93afbb968e43c28e48
Toolf <nazar.smirnov.2001@gmail.com>
2023-03-19 13:13:08 +0200

    DS-009: add: note api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,6 +2,7 @@ import '../db/db.dart';
 import 'action/action_api.dart';
 import 'book/book_api.dart';
 import 'character/character_api.dart';
+import 'note/note_api.dart';
 import 'relationship/relationship_api.dart';
 
 class Api {
@@ -9,12 +10,14 @@ class Api {
   final CharacterApi character;
   final ActionApi action;
   final RelationshipApi relationship;
+  final NoteApi note;
 
   Api._({
     required this.book,
     required this.character,
     required this.action,
     required this.relationship,
+    required this.note,
   });
 
   factory Api() {
@@ -23,6 +26,7 @@ class Api {
       character: CharacterApi(db.characterDataSource),
       action: ActionApi(db.actionDataSource),
       relationship: RelationshipApi(db.relationshipDataSource),
+      note: NoteApi(db.noteDataSource),
     );
   }
 }
---
+++ lib/api/note/note_api.dart
@@ -0,0 +1,33 @@
+import '../../core/crudl_api.dart';
+import '../../db/note_datasource.dart';
+import '../../domain/note/note.dart';
+import '../../domain/note/note_create.dart';
+import '../../domain/note/note_update.dart';
+import '../../schema/note/note.dart';
+import '../../schema/note/note_create.dart';
+import '../../schema/note/note_update.dart';
+
+class NoteApi {
+  final CrudlApi<Note, NoteCreate, NoteUpdate> _crudl;
+
+  get create => _crudl.create;
+  get read => _crudl.read;
+  get update => _crudl.update;
+  get delete => _crudl.delete;
+  get list => _crudl.list;
+
+  NoteApi._(
+    NoteDataSource dataSource,
+  ) : _crudl = CrudlApi<Note, NoteCreate, NoteUpdate>(
+          datasource: dataSource,
+          entitySchema: noteSchema,
+          entityUpdateSchema: noteUpdateSchema,
+          entityCreateSchema: noteCreateSchema,
+        );
+
+  factory NoteApi(NoteDataSource dataSource) {
+    return NoteApi._(
+      dataSource,
+    );
+  }
+}
